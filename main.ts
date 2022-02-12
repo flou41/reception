397b5a3aff0cafcb607d116f09f194d02ac8310c
flou41 <98581197+flou41@users.noreply.github.com>
2022-02-12 16:36:12 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,34 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    x = receivedNumber
+})
+radio.onReceivedValue(function (name, value) {
+    y = value
+})
+let y = 0
+let x = 0
+radio.setGroup(2)
 basic.forever(function () {
-	
+    if (x == 0 && y < 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, Math.abs(y))
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Math.abs(y))
+    } else if (x == 0 && y > 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, y)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, y)
+    } else if (x < 0 && y == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Math.abs(x))
+    } else if (x > 0 && y == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, x)
+    } else if (x < 0 && y < 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Math.abs(x))
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, x - Math.abs(y))
+    } else if (x < 0 && y > 0) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Math.abs(x))
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, x - Math.abs(y))
+    } else if (x > 0 && y == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, x)
+    } else if (x > 0 && y == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, x)
+    } else if (x == 0 && y == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    }
 })
